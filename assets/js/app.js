081b764befa71ac019c9d1af037accf950b64828
bryantjm <bryantjm@iupui.edu>
2013-04-23 16:37:08 -0400

    added geolocation

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $("#infoButton").click(function(){
 
 
-$("#map").toggle(function(show){
+$("#map").toggle(function(showOrHide){
 var mapProp = {
   center:new google.maps.LatLng(51.508742,-0.120850),
   zoom:5,
@@ -14,6 +14,50 @@ var map=new google.maps.Map(document.getElementById("map")
 
 console.log("map")
 });
+});
 
-	});
+
+
+$("#infoButton2").click(function(){
+var initialLocation;
+var siberia = new google.maps.LatLng(60, 105);
+var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var browserSupportFlag =  new Boolean();
+
+  var myOptions = {
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+  
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+  
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = newyork;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      initialLocation = siberia;
+    }
+    map.setCenter(initialLocation);
+  }
+  console.log("locate");
+
+
+});
 });
